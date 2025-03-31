9ac103f1f3ace29f587f12fd97d34c266e342278
ChrisLavin04 <S00251315@atu.ie>
2025-03-31 11:10:54 +0100

    Add project files.

---
+++ DataManagement/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Labsheet31March;
+
+namespace DataManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BookingData db = new BookingData();
+
+            using(db)
+            {
+                Customer c1 = new Customer() { Name = "Tom Jones", ContactNumber = "086-123 4567" };
+                Customer c2 = new Customer() { Name = "Mary Smith", ContactNumber = "086 546 3214" };
+                Customer c3 = new Customer() { Name = "Jo Doyle", ContactNumber = "087 1221 222" };
+
+                Console.WriteLine("Customers Created");
+
+                db.Customers.Add(c1);
+                db.Customers.Add(c2);
+                db.Customers.Add(c3);
+
+                Console.WriteLine("Added Customers to database");
+
+                db.SaveChanges();
+
+                Console.WriteLine("Changes Saved.");
+            }
+        }
+    }
+}
---
+++ Labsheet31March/Booking.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labsheet31March
+{
+    public class Booking
+    {
+        public int BookingId { get; set; }
+        public DateTime BookingsDate { get; set; }
+        public int NumberOfParticipants { get; set; }
+        public virtual Customer Customer { get; set; }
+    }
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+
+        public string ContactNumber { get; set; }
+        public virtual List<Booking> Bookings { get; set; }
+    }
+
+    public class BookingData : DbContext
+    {
+        public BookingData() : base("MyCustomerData") { }
+
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
